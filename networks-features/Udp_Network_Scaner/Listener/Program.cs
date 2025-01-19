bc978d1689138517c0a474fff4be919f4d2d427d
Алексей Дмитроченко <user012@as.local>
2025-01-19 21:03:31 +0700

    start adding Udp_Network_Scaner

---
+++ networks-features/Udp_Network_Scaner/Listener/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int listenPort = 11000;  
+
+StartListener();
+Console.WriteLine("Stop Listening");
+return;
+
+
+static void StartListener()  
+{ 
+	UdpClient listener = new UdpClient(listenPort) {EnableBroadcast = true};  
+	IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);  
+	//IPEndPoint groupEP = new IPEndPoint(IPAddress.Parse("192.168.1.35"), listenPort);  
+  
+	try  
+	{  
+		while (true)  
+		{  
+			Console.WriteLine("Waiting for broadcast");  
+			byte[] bytes = listener.Receive(ref groupEP);  
+  
+			Console.WriteLine($"Received broadcast from {groupEP} :");  
+			Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");  
+		}  
+	}  
+	catch (SocketException e)  
+	{  
+		Console.WriteLine(e);  
+	}  
+	finally  
+	{  
+		listener.Close();  
+	}  
+} 
---
+++ networks-features/Udp_Network_Scaner/Sender/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp){EnableBroadcast = true};  
+  
+IPAddress broadcast = IPAddress.Parse("192.168.1.255");  
+  
+byte[] sendbuf = Encoding.ASCII.GetBytes("Hello World!");  
+IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+
+while (true)
+{
+	s.SendTo(sendbuf, ep);
+	await Task.Delay(1000);
+	Console.WriteLine("Sending message to listener");
+}
+
